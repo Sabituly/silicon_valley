ec6ddd2882fc20b99305edaf42d7c96a440ed52d
Daniyar <ichbindaniyar@gmail.com>
2023-09-29 18:47:26 +0600

    Переписал toDoList через js код

--- 130923/public/script.js
+++ 130923/public/script.js
@@ -1,7 +1,6 @@
-let _toDoList, _inputTitleList;
-_toDoList = document.getElementById("toDoList");
-_inputTitleList = document.getElementById("inputTitleList");
-// _addButton = document.getElementById("addTask");
+let _toDoList = document.getElementById("toDoList"),
+    _inputTitleList = document.getElementById("inputTitleList");
+
 
 function _addTitleList() {
 
@@ -9,55 +8,99 @@ function _addTitleList() {
         alert("Введите название ваших дел");
         return;
     }
-    let nav = document.createElement("nav");
-    let randomNumber = Math.ceil(Math.random());
-    let _randomId = randomNumber + "randomId";
 
-    console.log(_randomId);
+    //создание элементов
+    let wrapperOfMenuTasks = document.createElement("div");
+    let newTasks = document.createElement("div");
+    let buttonDeleteTitleList = document.createElement("button");
+    let h3TitleList = document.createElement("h3");
+    let inputTitleTask = document.createElement("input");
+    let buttonAddTask = document.createElement("button");
 
-    nav.innerHTML = `<div class="nav-menu" id="newTasks">
-                     <button id="deleteTitleList" class="deleteTitleList" onclick="deleteTitleList(this)">X</button>
-                     <h3 class="nav-menu-h3" id="titleList">${_inputTitleList.value}</h3>
-                     <div class="new-tasks">
-                         <input type="text" placeholder="Введите задачу" class="inputText" id="inputTask">
-                         <button class="add" onclick="_addTask(`${_randomId}`)">`;
-    _toDoList.appendChild(nav);
-    console.log(_toDoList);
-    _inputTitleList.value = "";
+    newTasks.appendChild(inputTitleTask);
+    newTasks.appendChild(buttonAddTask);
+    wrapperOfMenuTasks.appendChild(h3TitleList);
+    wrapperOfMenuTasks.appendChild(buttonDeleteTitleList);
+    wrapperOfMenuTasks.appendChild(newTasks);
+    _toDoList.appendChild(wrapperOfMenuTasks);
+
+    //свойства элементов
+    inputTitleTask.type = "text";
+    wrapperOfMenuTasks.className = "nav-menu";
+
+    buttonDeleteTitleList.classList.add("deleteTitleList");
+    buttonDeleteTitleList.textContent = "X";
+    h3TitleList.classList.add("nav-menu-h3");
+    h3TitleList.textContent = _inputTitleList.value;
+    newTasks.classList.add("new-tasks");
+
+    inputTitleTask.classList.add("inputText");
+    inputTitleTask.id = "inputTitleTask";
+    inputTitleTask.placeholder = "Введите задачу";
+    buttonAddTask.classList.add("add");
+    buttonAddTask.textContent = "add";
+    // функции к элементам
+
+    buttonDeleteTitleList.setAttribute("onclick", "deleteTitleList(this)");
+    buttonAddTask.setAttribute("onclick","_addTask(this)"); // this значит эту кнопку
+    _inputTitleList.value = "";    //
 
 }
-function _addTask(inputElement) {
+function _addTask(button) {
 
-   let _ulMenu = document.getElementById("ulMenu");
+    //Переменные
+
+    let _ulMenu = document.createElement("ul");
+    let li = document.createElement("li");
+    let inputCheckBox = document.createElement("input");
+    let wrapperOfMenuTasks = button.closest(".nav-menu"); // ищет ближайщий элемент с заданными параметрами
+    let inputTitleTask = button.parentElement.querySelector('input[type="text"]');
+
+    let taskDescription = inputTitleTask.value;
+    let divInputTitleValue = document.createElement("div");
+    let buttonDeleteTask = document.createElement("button");
+
+    // свойства переменных
+
+    li.classList.add("new-tasks");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.classList.add("checkbox");
+    divInputTitleValue.textContent = inputTitleTask.value;
+    buttonDeleteTask.classList.add("delete");
+    divInputTitleValue.textContent = taskDescription;
+
+    buttonDeleteTask.textContent = "delete";
+    inputTitleTask.classList.add("inputText");
+    //
 
 
-    if(inputElement.value === "") {
+    //функции элементов
+    buttonDeleteTask.setAttribute("onclick", "deleteTask(this)");
+    if(inputTitleTask.value === "") {
         alert("Введите новую задачу");
         return;
     }
 
-    let li = document.createElement("li");
-    li.innerHTML = `<input type="checkbox" name = "first-task"> ${inputElement.value} <button class="delete" onclick="deleteTask(this)">delete</button>`;
+    inputTitleTask.value = "";
+
+    li.appendChild(inputCheckBox);
+    li.appendChild(divInputTitleValue);
+    li.appendChild(buttonDeleteTask);
+
+    // this.nextSibling.appendChild(li);
     _ulMenu.appendChild(li);
-    inputElement.value = "";
+    wrapperOfMenuTasks.appendChild(_ulMenu);
 }
 
-function deleteTask() {
 
-    let _ulMenu = document.getElementById("ulMenu");
-    _ulMenu.parentElement.removeChild(_ulMenu);
+function deleteTask(button) {
+
+    let li = button.parentElement;
+    li.remove();
 }
 
-// function deleteTitleList(button) {
-//     let _newTasks = button.closest('.nav-menu');
-//     if (_newTasks) {
-//         _newTasks.parentElement.removeChild(_newTasks);
-//     } else {
-//         console.error('Parent element not found.');
-//     }
-// }
 
-function deleteTitleList() {
-    let _newTasks = document.getElementById("newTasks");
-    _newTasks.parentElement.removeChild(_newTasks);
-}
+function deleteTitleList(button) {
+    let _newTasks = button.parentElement;
+    _newTasks.remove();
+};
